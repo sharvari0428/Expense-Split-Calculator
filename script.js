081b409c00c01285f1f6153ce8000e9957a90785
sharvari0428 <sharvaridhende06@gmail.com>
2026-06-14 13:24:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function calculateSplit(){
 
+let expense =
+document.getElementById("expense").value;
+
+let people =
+document.getElementById("people").value;
+
+if(expense === "" || people === ""){
+document.getElementById("answer").innerHTML =
+"Please enter all details";
+return;
+}
+
+let result =
+expense / people;
+
+document.getElementById("answer").innerHTML =
+"Each person should pay ₹" +
+result.toFixed(2);
+}
